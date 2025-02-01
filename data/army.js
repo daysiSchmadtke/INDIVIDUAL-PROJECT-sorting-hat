4233b4d7acbf9794a0d1c5e7dcccb320dd83b8be
Daysi Schmadtke <daysischmadtke@gmail.com>
2025-01-31 23:45:56 -0500

    created data structures

---
+++ data/army.js
@@ -0,0 +1,63 @@
+const army = [
+  {
+    id: 1,
+    name: "Belatrix Lestrange",
+    img: "https://www.usmagazine.com/wp-content/uploads/helena-bonham-carter-bellatrix-lestrange-harry-potter-zoom-89041266-aad6-47d6-817e-d9d2f57ff6c4.jpg?w=650&quality=86&strip=all",
+    member: "Death Eaters",
+    voldyArmy: true,
+  },
+  {
+    id: 2,
+    name: "Lucius Malfoy",
+    img: "https://static.hbo.com/content/dam/hbodata/series/game-of-thrones/character/joffrey-baratheon-1920.jpg",
+    voldyArmy: true,
+  },
+  {
+    id: 3,
+    name: "Peter Pettigrew",
+    img: "https://static.wikia.nocookie.net/omegaverse/images/e/e7/Peter_Pettigrew.jpg/revision/latest?cb=20240719203846",
+    voldyArmy: true,
+  },
+  {
+    id: 4,
+    name: "Esther Rookwood",
+    img: "https://static.wikia.nocookie.net/orphan/images/5/5f/MOV_Pre_Orphan2_2171.jpg/revision/latest/scale-to-width-down/250?cb=20120729172459",
+    voldyArmy: true,
+  },
+  {
+    id: 5,
+    name: "Morgana Yaxley",
+    img: "https://static.wikia.nocookie.net/heroes-and-villain/images/c/cf/Profile_-_Morgana_Pendragon.png/revision/latest?cb=20220817222106",
+    voldyArmy: true,
+  },
+  {
+    id: 6,
+    name: "Narcissa Malfoy",
+    img: "https://www.meme-arsenal.com/memes/027f64e1a57659de6740339d11e011bd.jpg",
+    voldyArmy: true,
+  },
+  {
+    id: 7,
+    name: "Winifred Sanderson",
+    img: "https://media.npr.org/assets/img/2018/11/30/4113_d011_00381_r1543365054_custom-2f6e39e8829ff641fee1cc49061533fbda4594fe.jpg",
+    voldyArmy: true,
+  },
+  {
+    id: 8,
+    name: "Mariam Karkaroff",
+    img: "https://i.pinimg.com/1200x/b7/1c/9b/b71c9b2119783ec978c2afce6ee0a8f6.jpg",
+    voldyArmy: true,
+  },
+  {
+    id: 9,
+    name: "Ludoviko Peluche",
+    img: "https://yt3.googleusercontent.com/ytc/AIdro_n9Fy3V2785aGU0XpnRNjA11WbZUVKUqsIdAqCjRz2Z9w=s900-c-k-c0x00ffffff-no-rj",
+    voldyArmy: true,
+  },
+  {
+    id: 10,
+    name: "Teresa Chavez",
+    img: "https://64.media.tumblr.com/8e778a1759ce16ec7ed6a043d638ea30/faf6c1573d42c39c-e5/s1280x1920/0f746f637e0e6e22d4491b3434afaae4d22477bd.jpg",
+    voldyArmy: true,
+  },
+];
